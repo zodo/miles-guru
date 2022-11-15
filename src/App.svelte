9cb0ec555d108d3fc75cf888d6bd75bce88a938c
Egor Ilchenko <zzodoo@gmail.com>
2022-11-15 21:05:24 +0400

    Markup

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,42 @@
-<script>
-  import Counter from './lib/Counter.svelte'
-  import '@picocss/pico'
-</script>
-
 <main class="container">
-  <h1>Hello, world!</h1>
-  <Counter />
+  <hgroup>
+    <h1>Miles guru</h1>
+    <h2>Оптимизация компенсации милями в Тинькофф банке</h2>
+  </hgroup>
+
+  <article>
+    <div class="grid">
+      <div>
+        <label for="copypasted">Скопируйте содержимое страницы Мили</label>
+        <textarea name="copypasted" placeholder="Тут текст" rows="10" />
+      </div>
+      <div>
+        <h5>Результат разбора</h5>
+        <h6>Доступно миль 43 124</h6>
+
+        <h6>Транзакции</h6>
+        <div>
+          <b>City Travel</b>
+          <small>8 035,00 ₽ / 9 000 миль</small>
+        </div>
+        <div>
+          <b>AviaKassa.com</b>
+          <small>22 696,00 ₽ / 24 000 миль</small>
+        </div>
+      </div>
+    </div>
+  </article>
+
+  <article>
+    <h3>Максимизируем рубли</h3>
+    <div>
+      <b>City Travel</b>
+      <small>8 035,00 ₽ / 9 000 миль</small>
+    </div>
+    <div>
+      <b>AviaKassa.com</b>
+      <small>22 696,00 ₽ / 24 000 миль</small>
+    </div>
+    <i>3 покупки на сумму 34 988 ₽ за 38 000 миль</i>
+  </article>
 </main>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-import './app.css'
 import App from './App.svelte'
+import './app.css'
+import '@picocss/pico'
 
 const app = new App({
   target: document.getElementById('app')
